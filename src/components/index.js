a8f8725f371b9a136f8cd025d798448018d2deb7
pawan0110 <meroy0075@gmail.com>
2025-09-09 22:10:19 +0530

    add header

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import Header from "./Header/Header";
+import Button from "./Button";
+import Input from "./Input";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+export {
+    Header,
+    Button,
+    Container,
+    Input,
+    LogoutBtn
+}
